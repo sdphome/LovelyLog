6148d2eaf38df9ba8d599e018fdf008b534140cf
sdphome <sdphome@qq.com>
2016-02-03 18:15:48 +0800

    StyledTextCtrl: display line mumber
    Signed-off-by: sdphome <sdphome@qq.com>

--- src/ll_textwin.py
+++ src/ll_textwin.py
@@ -2,13 +2,18 @@
 import wx
 import wx.stc
 
+# Margins
+MARK_MARGIN = 0
+NUM_MARGIN  = 1
+FOLD_MARGIN = 2
+
 class LlStc(wx.stc.StyledTextCtrl):
     def __init__(self, parent, id=wx.ID_ANY,
                  pos=wx.DefaultPosition, size=wx.DefaultSize, style=0):
-        super(LlStc, self).__init__(self, parent, id, pos, size, style)
+        super(LlStc, self).__init__(parent, id, pos, size, style)
 
         self.SetMarginType(MARK_MARGIN, wx.stc.STC_MARGIN_SYMBOL)
-        self.SetMarginMask(MARK_MARGIN, EditraBaseStc.ED_STC_MASK_MARKERS)
+        self.SetMarginMask(MARK_MARGIN, ~wx.stc.STC_MASK_FOLDERS)
         self.SetMarginSensitive(MARK_MARGIN, True)
         self.SetMarginWidth(MARK_MARGIN, 16)
 
@@ -19,8 +24,9 @@ class LlStc(wx.stc.StyledTextCtrl):
         self.SetMarginMask(FOLD_MARGIN, wx.stc.STC_MASK_FOLDERS)
         self.SetMarginSensitive(FOLD_MARGIN, True)
 
-class LlTextWin(wx.Panel):
-    def __init__(self, parent):
-        super(LlTextWin, self).__init__(parent, style=wx.SUNKEN_BORDER)
+class LlTextWin(wx.PyPanel):
+    def __init__(self, parent, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize,
+                 style=0, name="TextWindow"):
+        super(LlTextWin, self).__init__(parent, id, pos, size, style)
         self.SetBackgroundColour("white")
-        #self.stc = LlStc(self)
+        self.stc = LlStc(self)
